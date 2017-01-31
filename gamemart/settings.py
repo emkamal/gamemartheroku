947a2093d3e3684cf952e8871ef1b3122e6713b3
Kamal <mustafakamal87@gmail.com>
2017-01-31 18:56:52 +0200

    heroku setup

--- gamemart/settings.py
+++ gamemart/settings.py
@@ -126,3 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Only when running in Heroku
+if "DYNO" in os.environ:
+    STATIC_ROOT = 'staticfiles'
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    DEBUG = True # False, once service is succesfully deployed
+    ALLOWED_HOSTS = ['*']
