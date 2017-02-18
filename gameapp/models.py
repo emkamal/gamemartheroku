dd1185411cd5e9ee71fe1dec194b735f8b5d6515
Kamal <mustafakamal87@gmail.com>
2017-02-18 15:56:50 +0200

    deactivate JSONField

--- gameapp/models.py
+++ gameapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import JSONField
+# from django.contrib.postgres.fields import JSONField
 
 class Taxonomy(models.Model):
     taxonomy_type = models.CharField(max_length=20)
