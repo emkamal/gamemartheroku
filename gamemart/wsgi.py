a821533d3edd2865ae71793f9125aaa45d7b65a8
Kamal <mustafakamal87@gmail.com>
2017-02-18 14:56:24 +0200

    moving whitenoise import

--- gamemart/wsgi.py
+++ gamemart/wsgi.py
@@ -10,13 +10,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamemart.settings")
 
 # application = get_wsgi_application()
 if "DYNO" in os.environ:
     from dj_static import Cling
+    from whitenoise.django import DjangoWhiteNoise
     application = Cling(get_wsgi_application())
     application = DjangoWhiteNoise(application)
 else:
