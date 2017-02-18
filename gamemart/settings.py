5ae8a41e5fd612570cf64e5723f9fb65f22fc5de
Kamal <mustafakamal87@gmail.com>
2017-02-18 16:15:12 +0200

    revert middleware_classes to middleware

--- gamemart/settings.py
+++ gamemart/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     #'social.apps.django_app.default',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
