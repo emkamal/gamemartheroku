58b9ef41083a6f1da9cf3a2558e9a174273b7fa4
Kamal <mustafakamal87@gmail.com>
2017-02-19 22:20:05 +0200

    add something in urls

--- gamemart/urls.py
+++ gamemart/urls.py
@@ -52,3 +52,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
